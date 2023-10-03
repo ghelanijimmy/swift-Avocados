bf27e79cf0d5c649b6960ca63748f49c9c242d71
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-03 15:40:15 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Jimmy Ghelani on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
